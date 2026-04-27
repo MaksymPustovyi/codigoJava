cdad046c89e3195bb6e69a39fb057c3217c95157
Maksym Pustovyi <maksym.s.pustovyi@gmail.com>
2026-04-27 12:48:24 +0200

    Añadido Ejercicio 2

--- Introduccion a Java/Ejercicios/Ejer1Iniciacion.java
+++ Introduccion a Java/Ejercicios/Ejer1Iniciacion.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
- * Pagina 184, Introducción a JAVA.pdf 
+ * Pagina 184, Introducción a JAVA.pdf
  * Ejercicio 1
  * Leer una cantidad 'N' y luego introducir 'N' números enteros.
  * Se pide imprimir el mayor y el menor y las veces que aparece cada uno.
@@ -12,22 +12,22 @@ public class Ejer1Iniciacion {
 
         Scanner entrada = new Scanner(System.in);
         int cantidadNumeros, numActual;
-        int contador = 0, max = 0, min = 0, contadorMax = 0, contadorMin = 0;
+        int contador = 1, max = 0, min = 0, contadorMax = 0, contadorMin = 0;
 
         // leemos una cantidad 'N'
-        
+
         System.out.println("\n- INICIO DEL EJERCICIO -\n");
-        
+
         System.out.print("Cuantos numeros quieres introducir? ");
         cantidadNumeros = entrada.nextInt();
         System.out.println("--------------------");
 
         // un bucle para leer y analizar la cantidad 'N' de numeros
         while (contador < cantidadNumeros) {
-            System.out.print("Introduce un numero " + (contador + 1) + " de " + cantidadNumeros +": ");
+            System.out.print("Introduce un numero " + (contador + 1) + " de " + cantidadNumeros + ": ");
             numActual = entrada.nextInt();
             System.out.println("--------------------");
-            
+
             // si es primero numero >>> inicializamos max y min
             if (contador == 0) {
                 max = numActual;
@@ -46,7 +46,7 @@ public class Ejer1Iniciacion {
                     contadorMax++;
                 }
 
-                //nuevo minimo
+                // nuevo minimo
                 if (numActual < min) {
                     min = numActual;
                     contadorMin = 1;
@@ -62,9 +62,11 @@ public class Ejer1Iniciacion {
         }
 
         // imprimimos los resultados
-        System.out.println("\nEl minimo es [" + min + "] y aparece (" + contadorMin + (contadorMin == 1 ? ") vez" : ") veces"));
-        System.out.println("El maximo es [" + max + "] y aparece (" + contadorMax + (contadorMax == 1 ? ") vez" : ") veces"));
-        
+        System.out.println(
+                "\nEl minimo es [" + min + "] y aparece (" + contadorMin + (contadorMin == 1 ? ") vez" : ") veces"));
+        System.out.println(
+                "El maximo es [" + max + "] y aparece (" + contadorMax + (contadorMax == 1 ? ") vez" : ") veces"));
+
         System.out.println("\n- FIN DEL EJERCICIO -\n");
 
         entrada.close();
