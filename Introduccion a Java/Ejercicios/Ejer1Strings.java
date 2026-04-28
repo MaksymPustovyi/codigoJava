686b4dbb4474645d21e3f633098148eede9697b6
Maksym Pustovyi <maksym.s.pustovyi@gmail.com>
2026-04-28 10:35:04 +0200

    Ejercicio 2 Strings

---
+++ Introduccion a Java/Ejercicios/Ejer1Strings.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/**
+ * Pagina 185, Introducción a JAVA.pdf
+ * Ejercicio 1 Strings
+ * Crea un programa que te pida una palabra y escriba las letras separadas por
+ * espacios.
+ * Ejemplo, a partir de "Pepe" escribirá "P e p e ".
+ */
+
+public class Ejer1Strings {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        // inicializamos el ejercicio
+        System.out.println("\n- INICIO DEL EJERCICIO -\n");
+
+        System.out.print("Introduce una palabra: ");
+        String palabra = entrada.next(); // leemos una palabra del usuario
+
+        // imprimimos el resultado
+        System.out.println("--------------------");
+        System.out.print("Resultado de la conversion: [");
+
+        // сonvertimos el String a un array y concatenamos un espacio a cada carácter y imprimimos resultado en una linea
+        for (char letra : palabra.toCharArray()) {
+            System.out.print(letra + " ");
+        }
+        System.out.println("\b]"); // borra el último carácter sobrante (como un espacio o coma) y cierra el formato con ']'.
+        System.out.println("--------------------");
+
+        System.out.println("\n- FIN DEL EJERCICIO -\n");
+
+        entrada.close();
+    }
+}
---
+++ Introduccion a Java/Ejercicios/Ejer2Strings.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/**
+ * Pagina 185, Introducción a JAVA.pdf
+ * Ejercicio 2 Strings
+ * Crea un programa en JAVA que reciba 5 palabras y luego nos muestre la que tiene una longitud mayor.
+ */
+
+public class Ejer2Strings {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        String palabra = "", palabraMax = "";
+
+        // inicializamos el ejercicio
+        System.out.println("\n- INICIO DEL EJERCICIO -\n");
+
+        for (int i=1; i<=5; i++) {
+        System.out.print("Introduce " + (i==1 ? "primera" : i==2 ? "segunda" : i==3 ? "tercera" : i==4 ? "cuarta" : "quinta") + " palabra: ");
+        palabra = entrada.nextLine(); // leemos una palabra del usuario
+
+        // si la palabra actual es estrictamente más larga que la guardada, actualiza el valor.
+        if (palabra.length() > palabraMax.length())
+            palabraMax = palabra;
+
+        }
+        // imprimimos el resultado
+        System.out.println("--------------------");
+        System.out.println("La primera palabra más larga es: [" + palabraMax + "]");
+        System.out.println("--------------------");
+
+        System.out.println("\n- FIN DEL EJERCICIO -\n");
+
+        entrada.close();
+    }
+}
